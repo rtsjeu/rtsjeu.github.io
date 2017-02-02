fdc35bbed4d0e6be9491fde1d0b6af798fb874e6
sebastien <sebastien.chauvin@gmail.com>
2017-02-02 20:29:19 +0100

    convert and original data

--- convert.py
+++ convert.py
@@ -8,47 +8,19 @@ import time
 from pprint import pprint
 
 import os
-from slackclient import SlackClient
 
-
-if ('SLACK_BOT_TOKEN' in os.environ):
-	speed = 1
-else:
-	speed = 0
-
-def send(str):
-	if ('SLACK_BOT_TOKEN' in os.environ):
-		slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
-		slack_client.api_call("chat.postMessage", channel="geneva", text=str, as_user=True)
-	else:
-		pprint(str)
 
 with open('data.json') as data_file:
 	data = json.load(data_file)
 
-i = random.randrange(len(data))
+output = {'version': str(time.time()), 'name': 'lejeu', 'questions': []}
+for d in data:
+    q = {}
+    q['text'] = d['Question']
+    q['answers'] = []
+    q['answers'].append({'text': d['Reponse1'], 'isCorrect': True})
+    q['answers'].append({'text': d['Reponse2'], 'isCorrect': False})
+    q['answers'].append({'text': d['Reponse3'], 'isCorrect': False})
+    output['questions'].append(q)
 
-send(str(i) + ": " + data[i]['Categorie'] + "/" + data[i]['SousCategorie'])
-
-send(data[i]['Question'])
-
-time.sleep(10*speed)
-
-a = random.randrange(1, 4)
-
-send(data[i]['Reponse'+str(a)])
-b = 2
-if (a == 2):
-	b = 1
-time.sleep(1*speed)
-send(data[i]['Reponse'+str(b)])
-c = 3
-if (a == 3):
-	c = 1
-time.sleep(1*speed)
-send(data[i]['Reponse'+str(c)])
-
-time.sleep(20*speed)
-send("Reponse: " + data[i]['Reponse1'])
-send(data[i]['Anecdote'])
-send(data[i]['Media'])
+print json.dumps(output)
